24bca524ecbd0622c12643e4924d8e94b580e584
qinscode <fudong.qin@outlook.com>
2024-07-09 05:54:25 +0800

    update port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,11 @@ module.exports = {
       args: 'start',
       env: {
         NODE_ENV: 'development',
+        PORT: 3000, // 更改为你希望的端口
       },
       env_production: {
         NODE_ENV: 'production',
+        PORT: 8001, // 更改为你希望的端口
       },
     },
   ],
