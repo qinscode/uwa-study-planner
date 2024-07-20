6fb2cd53076a8799bfe1bb00a2742d49a54d0baf
qinscode <fudong.qin@outlook.com>
2024-07-21 03:57:51 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 将输出目录更改为 build
+    rollupOptions: {
+      output: {
+        // 自定义 chunk 的拆分策略
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // 调整 chunk size 警告限制
+  },
 })
